2879defe859430491b54bc27414ab66c9748c9e5
Daniel Golman <aircertio@gmail.com>
2018-10-23 11:15:38 -0400

    prepare for submission

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 // const rp = require('request-promise');
 const fs = require('fs');
 const cheerio = require('cheerio');
+
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const csvWriter = createCsvWriter({
+  path: './csv/shows.csv',
+  header: [
+    {id: 'event', title: 'EVENT'},
+    {id: 'description', title: 'DESCRIPTION'},
+    {id: 'from_url', title: 'FROM_URL'},
+    {id: 'date', title: 'DATE'},
+    {id: 'times', title: 'TIMES'},
+    {id: 'image', title: 'IMAGE'},
+    {id: 'reservation_url', title: 'RESERVATION_URL'}
+  ]
+});
 // offline read because firewall issue
 var uri = "https://www.930.com";
 $ = cheerio.load(fs.readFileSync('./9_30.html'));
@@ -18,16 +32,19 @@ $('.upcoming-shows').find('#upcoming-listview').find('.list-view-item').each(fun
 	show = $(show);
 	shows.push({
 		event: show.find('.headliners a').text(),
-		description: show.find('.descriptions a').text(),
+		description: show.find('.description a').text(),
 		from_url: show.find('a').attr('href'),
 		date: show.find('.dates').text(),
 		times: show.find('.times').text(),
-		img: removeCurrentDir(show.find('a').find('img').attr('src')),
+		image: removeUndefined((removeCurrentDir(show.find('a').find('img').attr('src')))),
 		reservation_url: removeUndefined(show.find('.ticket-link .tickets').attr('href'))
 	});
 });
-
-console.log(shows);
+ 
+csvWriter.writeRecords(shows)       // returns a promise
+  .then(() => {
+  	console.log('...Done');
+ });
 
 function removeCurrentDir(url) {
 	// Only replace the first instance;
@@ -35,7 +52,7 @@ function removeCurrentDir(url) {
 }
 
 function removeUndefined(str) {
-	return str ? str : '';
+	return str ? str : 'Not Found';
 }
 
 
